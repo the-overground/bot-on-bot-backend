0157525dea596747d41372c2df949c03f4391780
nilllzz <nidresch@googlemail.com>
2017-02-26 06:18:37 +0100

    add base .net core app

---
+++ src/BotOnBot.Backend/BotOnBot.Backend/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BotOnBot.Backend
+{
+    internal static class Program
+    {
+        internal static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
